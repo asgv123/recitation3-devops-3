69f22ad01f23750dc3f3ce09676f2aebad89fefd
vedangj-cmu <vedangj@andrew.cmu.edu>
2025-11-03 22:03:19 -0500

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    companies = [
+        {"name": "Nvidia", "valuation": "$4.23T"},  
+        {"name": "Microsoft", "valuation": "$4.04T"},  
+        {"name": "Apple", "valuation": "$3.99T"},  
+        {"name": "Alphabet", "valuation": "$2.58T"},  
+        {"name": "Amazon", "valuation": "$2.44T"}  
+    ]
+    return templates.TemplateResponse("index.html", {"request": request, "companies": companies})
